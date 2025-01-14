d55d8edec9447e7444ac0d47fb46c4e22e411173
Amangtt <amangtebeje123@gmail.com>
2025-01-14 17:41:58 +0300

    API files used to build and check by accessing the API endpoint

---
+++ check.py
@@ -0,0 +1,8 @@
+import requests
+
+url = "http://127.0.0.1:8000/predict"
+data = {"features": [4,5,1,1,1,2015,7,31,0,-1.011865674,1.071317226,3,0,1,1,0,0,0]}  # Replace with your actual input features
+
+
+response = requests.post(url, json=data)
+print(response.json())
