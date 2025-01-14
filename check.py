75a3f83a5981b90455d943607152f51223bbf1eb
Amangtt <amangtebeje123@gmail.com>
2025-01-14 17:43:36 +0300

    modifed

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "http://127.0.0.1:8000/predict"
-data = {"features": [4,5,1,1,1,2015,7,31,0,-1.011865674,1.071317226,3,0,1,1,0,0,0]}  # Replace with your actual input features
+data = {"features": [4,5,1,1,1,2015,7,31,0,-1.011865674,1.071317226,3,0,1,1,0,0,0]}  
 
 
 response = requests.post(url, json=data)
